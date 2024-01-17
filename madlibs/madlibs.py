42ba0e2f5f9c71f57e3b1133100ae29761b94289
aminMuktar <aminmuktar41@gmail.com>
2024-01-17 16:48:54 +0300

    madlib: basic string concatenation

---
+++ madlibs/madlibs.py
@@ -0,0 +1,14 @@
+youtuber = "amin"
+
+# print("Subscribe to "+youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("verb: ")
+famous_person = input("famous person")
+
+madlib = f"Computer programming is so {adj}!. It makes me so excited all the \
+    time because I love to {verb1}. stay hydrated and {verb2} like you are {famous_person}"
+print(madlib)
